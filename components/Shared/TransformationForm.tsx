8c9e8a6962b4adccb4489853dc58971baabedcf1
hRJ06 <hindolroy.2306@gmail.com>
2024-03-05 15:55:34 +0530

    Integrated Cloudinary with GEN - AI Integration, Upload Widget

--- components/Shared/TransformationForm.tsx
+++ components/Shared/TransformationForm.tsx
@@ -31,6 +31,7 @@ import { CustomField } from "./CustomField";
 import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updatedCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -93,10 +94,10 @@ const TransformationForm = ({
     setTransformationConfig(
       deepMergeObjects(newTransformation, transformationConfig)
     );
-    setNewTransformation(null)
-    startTransition(async() => {
+    setNewTransformation(null);
+    startTransition(async () => {
       /* await updatedCredits(userId,creditFee) */
-    })
+    });
   };
   const onSelectFieldHandler = (
     value: string,
@@ -199,6 +200,22 @@ const TransformationForm = ({
             )}
           </div>
         )}
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
         <div className="flex flex-col gap-4">
           <Button
             type="button"
