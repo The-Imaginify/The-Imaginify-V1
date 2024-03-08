716b0f0a5cadb73df1b2a2a983842f074e6c37d0
hRJ06 <hindolroy.2306@gmail.com>
2024-03-08 13:39:47 +0530

    Fixed A Minor Bug in Mobile Navigation

--- components/Shared/MobileNav.tsx
+++ components/Shared/MobileNav.tsx
@@ -78,7 +78,7 @@ const MobileNav = () => {
         </SignedIn>
         <SignedOut>
           <Button asChild className="button bg-purple-gradient bg-cover">
-            <Link href="/sign-in"></Link>
+            <Link href="/sign-in">Login</Link>
           </Button>
         </SignedOut>
       </nav>
