feb9812b9a5047d37b34dd927d9e0678718e6ff4
hRJ06 <hindolroy.2306@gmail.com>
2024-03-08 17:37:49 +0530

    Fixed a Minor Bug in Shimmer Effect

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,7 +22,7 @@ export const handleError = (error: unknown) => {
 };
 
 const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http:
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
       <stop stop-color="#7986AC" offset="20%" />
